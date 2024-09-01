833811654c3ffc189a04b189275cd5c64d1322f3
Steven <20220721@ricaldone.edu.sv>
2024-09-01 17:15:00 -0600

    .

--- src/modelo/UsuarioModelo.java
+++ src/modelo/UsuarioModelo.java
@@ -17,6 +17,62 @@ public class UsuarioModelo {
     private String ContrasenaUsuario;
     private String FotoUsuario;
     private Number IdRole;
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return NombreUsuario;
+    }
+
+    public void setNombreUsuario(String NombreUsuario) {
+        this.NombreUsuario = NombreUsuario;
+    }
+
+    public int getEdadUsuario() {
+        return EdadUsuario;
+    }
+
+    public void setEdadUsuario(int EdadUsuario) {
+        this.EdadUsuario = EdadUsuario;
+    }
+
+    public String getCorreoUsuario() {
+        return CorreoUsuario;
+    }
+
+    public void setCorreoUsuario(String CorreoUsuario) {
+        this.CorreoUsuario = CorreoUsuario;
+    }
+
+    public String getContrasenaUsuario() {
+        return ContrasenaUsuario;
+    }
+
+    public void setContrasenaUsuario(String ContrasenaUsuario) {
+        this.ContrasenaUsuario = ContrasenaUsuario;
+    }
+
+    public String getFotoUsuario() {
+        return FotoUsuario;
+    }
+
+    public void setFotoUsuario(String FotoUsuario) {
+        this.FotoUsuario = FotoUsuario;
+    }
+
+    public Number getIdRole() {
+        return IdRole;
+    }
+
+    public void setIdRole(Number IdRole) {
+        this.IdRole = IdRole;
+    }
     
     
     
